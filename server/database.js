cc3f4a189a7939c733174ce7dba603366521d852
DamjanP <pejkiovskid@gmail.com>
2026-06-01 20:14:44 +0200

    added auth.js to authenticate the tokens or return invalid. Also added ownerOnly.js for after token valiadation to validate if owner or not and enable owner routes

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
--- server/newUser.js
+++ server/newUser.js
@@ -3,9 +3,11 @@ const pool = require('./database');
 require('dotenv').config();
 
 async function addUsers() {
+    //hashing passwords for the two users
     const hashedPasswordOwner = await bcrypt.hash('passwordOwner123', 10);
     const hashedPasswordWorker = await bcrypt.hash('passwordWorker123', 10);
 
+    // queries for inserting the two users into the database
     await pool.query(
         `INSERT INTO USERS (name, email, password, role) VALUES ('Admin', 'admin@snackops.com', '${hashedPasswordOwner}', 'owner')`
     );
